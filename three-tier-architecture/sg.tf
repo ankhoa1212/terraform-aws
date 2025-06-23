32bc19ecacf66cde8e12b875b4a4e8712bc4fb16
Austin <ankhoa1212@gmail.com>
2025-06-22 20:51:31 -0700

    add security group setup

---
+++ three-tier-architecture/sg.tf
@@ -0,0 +1,34 @@
+# Web ALB Security Group
+resource "aws_security_group" "web_alb" {
+  name        = "web-alb-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Database Security Group
+resource "aws_security_group" "database" {
+  name        = "db-sg"
+  description = "Allow MySQL access from app tier"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [module.core.app_instance_sg_id]
+  }
+}
